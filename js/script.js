e41c10720440f456e968d0ae0dbfeda6eb8644c5
Ken Robinson <subscriptions@kodehaus.com>
2021-01-08 14:32:14 -0500

    Added background change, refresh and additional display count propery

--- js/script.js
+++ js/script.js
@@ -13,66 +13,52 @@ project 1 - A Random Quote Generator
 //declare an empty quotes array
 let quotes =[];
 
- // We need an origination source for the quote data
- const quoteStringArr = [
-   //multi-dimensional inner array pattern ['QUOTE','SOURCE','CITATION','YEAR']
-   ['hey lose the day in expectation of the night, and the night in fear of the dawn.', 'Seneca', '', ''],
-   ['Just keep in mind: the more we value things outside our control, the less control we have.', 'Epictetus', '', ''],
-   ['It does not matter what you bear, but how you bear it. ', 'Seneca','', ''],
-   ['Learn to be indifferent to what makes no difference.', 'Marcus Aurelius','', ''],
-   ['The more you seek the uncomfortable, the more you will become comfortable. ', 'Conor McGregor','', ''],
-   ['Everyone has a plan \'till they get punched in the mouth.', 'Mike Tyson','', ''],
-   ['Behind every great man is a woman rolling her eyes.', 'Jim Carey','', ''],
-   ['Any man can learn anything he will, but no man can teach except to those who want to learn', 'Henry Ford', 'Ford News','1924'],
-   ['Education is preeminently a matter of quality, not amount', 'Henry Ford', 'Ford News','1924']
- ];
+//set the value that determines the interval for randomly loading quotes
+const reloadInterval = 2500; //1000 = 1 second
+
+//load the quote objects
+loadQuoteObjects();
 
 
- //add  empty quote objects - matches the number of elements in the qquoteStringArr array
- for(let i = 0; i < quoteStringArr.length; i++){
-  quotes.push(createQuoteObject());
- }
+//randomly reload the quotes on set interval
+reloadPageOnInterval(reloadInterval);
 
- //populate quote objects - matched the number of elements in the squoteStringArr array
- for(let i = 0; i < quoteStringArr.length; i++){
-     //populate the objects with values from the multidimensional arrays
-     quotes[i].quotation = quoteStringArr[i][0];
-     quotes[i].source = quoteStringArr[i][1];
 
-     //ensure the field has a valid value otherwise it should remain undefined
-     if(quoteStringArr[i][2].length >= 3){
-         quotes[i].citation = quoteStringArr[i][2];
-      }
-     //ensure the field has a valid value otherwise it should remain undefined
-     if(quoteStringArr[i][3].length >= 4){
-         quotes[i].year = quoteStringArr[i][3];
-     }
-  }
+//use an arrow function to secure rgb values for altering the backgrounds
+const getRGBValue = (floor, ceiling) => Math.floor((Math.random() * (ceiling - floor)) + floor)+1;
 
-//reusable code to create empty or populated objects
-function createQuoteObject(quotation, source, citation, year){
-    //create an empty object than add properties and values to the object
-    let newQuotationObject = {};
 
-    newQuotationObject.quotation = quotation;
-    newQuotationObject.source = source;
-    newQuotationObject.citation = citation;
-    newQuotationObject.year = year;
 
-    return newQuotationObject;
+
+function loadQuoteObjects(){
+//add quote objects into the array
+   quotes.push({quotation: 'hey lose the day in expectation of the night, and the night in fear of the dawn.', source: 'Seneca', citation: '', year: '' });
+   quotes.push({quotation: 'Just keep in mind: the more we value things outside our control, the less control we have. ', displayCount: 0, source: 'Epictetus', citation: '', year: '' });
+   quotes.push({quotation: 'It does not matter what you bear, but how you bear it. ', source: 'Seneca',citation: '', year: '' });
+   quotes.push({quotation: 'Learn to be indifferent to what makes no difference.', source: ' Marcus Aurelius', displayCount: 0,citation: '', year: '' });
+   quotes.push({quotation: 'The more you seek the uncomfortable, the more you will become comfortable. ', displayCount: 0 , source: 'Conor McGregor',citation: '', year: '' });
+   quotes.push({quotation: 'Everyone has a plan \'till they get punched in the mouth.', source: 'Mike Tyson', displayCount: 0,citation: '', year: '' });
+   quotes.push({quotation: 'Behind every great man is a woman rolling her eyes.', source: 'Jim Carey', displayCount: 0});
+   quotes.push({quotation: 'Any man can learn anything he will, but no man can teach except to those who want to learn', displayCount: 0, source: 'Henry Ford',citation: '01/01/1924', year: 'Ford News' });
+   quotes.push({quotation: 'Education is preeminently a matter of quality, not amount', source: 'Henry Ford', displayCount: 0 ,citation: '01/01/1924', year: 'Ford News' });
 }
-
 /***
  * `getRandomQuote` function
 ***/
 function getRandomQuote(){
   //ensure the array length matches with the number of items in the array
   const randomQuoteNumber = Math.floor((Math.random() * quotes.length-1)+1);
-  console.log('randomQuoteNumber: ' + randomQuoteNumber);
-  return quotes[randomQuoteNumber];
+  let selectedQuote = quotes[randomQuoteNumber];
+
+  //ensure all objects have a displayCount property and variable, even if the orginal object doesn't
+  if(!selectedQuote.displayCount){
+    selectedQuote.displayCount = 1;
+  } else {
+    selectedQuote.displayCount ++;
+  }
+  return selectedQuote;
 }
 
-const getRGBValue = (floor, ceiling) => Math.floor((Math.random() * (ceiling - floor)) + floor)+1;
 
 /***
  * `printQuote` function
@@ -90,20 +76,31 @@ function printQuote(){
         html += `
           <span class="year">${quote.year}</span>`;
     }
-    html + "</p>";
+
+    //Displays the number of times the quote has been loaded.
+      html += `
+          </p>
+            <p>Display Count: <span>${quote.displayCount}</span>`;
+      html + "</p>";
 
     document.getElementById('quote-box').innerHTML = html; 
     changeBackgroundColor();
     changeQuoteFontColor();
 }
 
+// lets ensure we maintain contrast between the background color and the fonts
+// darker colors should be lower on the 255 scale and lighter numbers higher 
+// our backgrounds should always be darker than our fonts 
 function changeBackgroundColor(){
   document.body.style.backgroundColor = `rgb(${getRGBValue(0,100)}, ${getRGBValue(0,100)} ,${getRGBValue(0,100)})`;
 }
 function changeQuoteFontColor(){
-  document.body.style.color = `rgb(${getRGBValue(155,255)}, ${getRGBValue(155,255)} ,${getRGBValue(155,255)})`;  
+  document.body.style.color = `rgb(${getRGBValue(130,255)}, ${getRGBValue(130,255)} ,${getRGBValue(130,255)})`;  
 }
-
+//this function create the time interval
+function reloadPageOnInterval(interval){
+  window.setInterval(function(){printQuote()}, interval);
+}
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
